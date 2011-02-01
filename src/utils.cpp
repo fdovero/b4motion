9fbefac90d6c8083807939395dc7a16c4c2e2f8d
Fabien Dovero <fabien.dovero@gmail.com>
2011-02-01 13:01:34 +0100

    Added utils

---
+++ src/utils.cpp
@@ -0,0 +1,38 @@
+/*
+ *utils.cpp
+ *
+ *
+ *
+ */
+
+#include <utils.h>
+#ifdef __unix__
+	#include <time.h>
+	#include <sys/time.h>
+#else
+	#include <windows.h>
+#endif
+
+namespace b4m
+{
+	long long getTimeMillis()
+	{
+	#ifdef __unix__
+		// --------------- Sous GNU/Linux ---------------
+		// On récupère l'heure dans une structure "timeval"
+		timeval tv;
+		gettimeofday( &tv, NULL );
+		// On la convertit en millisecondes à partir des secondes et
+		// des microsecondes
+		long long l = tv.tv_usec / 1000;
+		l += tv.tv_sec * 1000;
+	#else
+		// ---------------- Sous Windows ----------------
+		// La fonction GetTickCount() de windows.h nous
+		// donne directement un timestamp en millisecondes
+		long long l= GetTickCount();
+	#endif
+
+	return l;
+	}
+}
---
+++ src/utils.h
@@ -0,0 +1,17 @@
+/*
+ *utils.h
+ *
+ *
+ *
+ */
+
+#ifndef B4MUTILS_H_
+#define B4MUTILS_H_
+
+namespace b4m
+{
+	/// Retourne l'heure actuelle en millisecondes
+	long long getTimeMillis();
+}
+
+#endif /* B4MUTILS_H_ */
